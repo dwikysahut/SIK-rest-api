cfe10df841ef600da0821021a7a9835eb0cceae9
Dwiky S <dwikysahut@gmail.com>
2023-12-07 16:21:04 +0700

    docker

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,11 +1,13 @@
-const db = require('mysql');
+const db = require('mysql2');
 const bluebird = require('bluebird');
 require('dotenv').config();
 
 const connection = db.createConnection({
   host: process.env.DB_HOST,
   user: 'root',
-  password: '',
+
+  port: process.env.DB_PORT,
+  password: '123',
   database: process.env.DB_NAME,
 });
 
